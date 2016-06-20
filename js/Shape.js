bcca6210ed8e27e35c583d11d146d883f6057c06
王锐 <mvpjly@163.com>
2016-06-20 11:22:53 +0800

    first commit

---
+++ js/Shape.js
@@ -0,0 +1,10 @@
+var Shape = {
+    cube: function () {},
+    pyramid: function () {},
+    cylinder: function () {},
+    prism: function () {},
+    sphere: function () {},
+    fan: function () {},
+    polyhedral: function () {},
+    ladder: function () {}
+}
